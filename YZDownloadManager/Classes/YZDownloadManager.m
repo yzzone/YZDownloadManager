36d3b7d3fdd36180d3e18f3385c80a9cac5fcef0
yzzone <393019996@qq.com>
2020-05-21 11:15:13 +0800

    remove note

--- YZDownloadManager/Classes/YZDownloadManager.m
+++ YZDownloadManager/Classes/YZDownloadManager.m
@@ -69,12 +69,6 @@
 
 #pragma mark - 下载操作相关方法
 
-/// 执行开始下载操作，可以指定是否执行优先下载
-/// @param objectArray 下载对象数据的集合
-/// @param block 下载被拒绝原因回调，rejectArray 回调中被拒原因数组
-/// @param rejectArray 附加下载被拒原因数组，主要是生成下载时由于某些原因（下载URL为空、类型不支持等）造成的，和block回调中的rejectArray合并返回
-/// @param isPriority 是否是要执行优先下载
-/// @param isNeedShowCommonTip 是否需要展示公共toast提示
 - (void)yz_startDownload:(NSMutableArray <YZDownloadObject *> *)objectArray
      downloadRejectBlock:(nullable void (^)(NSMutableArray <YZDownloadRejectModel *> * _Nullable rejectArray))block
         subjoinRejectionArray:(nullable NSMutableArray *)rejectArray
@@ -105,21 +99,14 @@
 
 #pragma mark - 下载对象的相关方法
 
-/// 通过下载URL获取下载对象，该方法只适用于通过URL执行的下载，如果没有匹配到数据，则返回nil
-/// @param url 下载地址
-/// @return 下载数据
 - (YZDownloadObject *)yz_getDownloadObjectByUrl:(NSString *)url {
     return nil;
 }
 
-/// 根据下载状态获取下载池中的下载对象集合
-/// @return 下载完成的下载对象集合
 - (NSMutableArray <YZDownloadObject *> *)co_getDownloadObjectArrayByDownloadState:(YZDownloadState)downloadState {
     return nil;
 }
 
-/// 通过缓存的下载文件名称查找下载模型数据
-/// @param localName 下载文件名称
 - (YZDownloadObject *)yz_getDownloadObjectByDownloadLocalName:(NSString *)localName {
     return nil;
 }
